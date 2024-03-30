14352481cc8a5960f0f0b434afa149fa32637f5a
Kiol-Ice <ramadiel2001@gmail.com>
2024-03-31 00:02:02 +0100

    image up/down

--- flask/index.py
+++ flask/index.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 index_bp = Blueprint("index_bp", __name__)
 
 @index_bp.route('/')
 def index():
-    return "<h3>Aws Segmentation app.</h3>"
+    return render_template('index.html')
--- flask/server.py
+++ flask/server.py
@@ -9,8 +9,10 @@ def register_blueprints(app: Flask, list_bp: List):
         app.register_blueprint(bp)
 
 def create_app():
+    UPLOAD_FOLDER = './flask/image_to_seg'
     app = Flask(__name__)
-
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    
     import secrets 
     
     app.secret_key = secrets.token_hex(128)
@@ -21,20 +23,22 @@ def create_app():
         logger.info("Starting Blueprints registration...")
         
         from index import index_bp
+        from segmentation import segmentation_bp
+        from download import download_bp
 
-        list_bp = [index_bp]
+        list_bp = [index_bp, segmentation_bp, download_bp]
 
         register_blueprints(app, list_bp)
         logger.info("Successfully registered Blueprints.")
     except:
         logger.critical("Unable to register Blueprints.", exc_info=traceback.format_exc())
     
-    try:
-        logger.info("Starting error handler registration...")
-        app.register_error_handler(404, page_not_found)
-        logger.info("Successfully registered error handler.")
-    except:
-        logger.critical("Unable to register error handler.", exc_info=traceback.format_exc())
+    # try:
+    #     logger.info("Starting error handler registration...")
+    #     app.register_error_handler(404, page_not_found)
+    #     logger.info("Successfully registered error handler.")
+    # except:
+    #     logger.critical("Unable to register error handler.", exc_info=traceback.format_exc())
     
     return app
 
