c8c7f7bc574393c46fdef5112778a912270b6b44
Kiol-Ice <ramadiel2001@gmail.com>
2024-03-31 12:50:32 +0200

    séparation flask

---
+++ outil_segmentation.py
@@ -0,0 +1,152 @@
+# System libs
+import os
+# Numerical libs
+import numpy as np
+import torch
+import torch.nn as nn
+from scipy.io import loadmat
+import csv
+# Our libs
+from mit_semseg.dataset import TestDataset
+from mit_semseg.models import ModelBuilder, SegmentationModule
+from mit_semseg.utils import colorEncode, find_recursive
+import logging
+from mit_semseg.lib.nn import user_scattered_collate
+from PIL import Image
+from mit_semseg.config import cfg
+
+def load_classes():
+    colors = loadmat('data/color150.mat')['colors']
+    names = {}
+    with open('data/object150_info.csv') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            names[int(row[0])] = row[5].split(";")[0]
+            
+    return colors, names
+
+def visualize_result(data, pred, cfg):
+    (img, info) = data
+    colors, names = load_classes()
+    
+    # print predictions in descending order
+    pred = np.int32(pred)
+    pixs = pred.size
+    uniques, counts = np.unique(pred, return_counts=True)
+    print("Predictions in [{}]:".format(info))
+    for idx in np.argsort(counts)[::-1]:
+        name = names[uniques[idx] + 1]
+        ratio = counts[idx] / pixs * 100
+        if ratio > 0.1:
+            print("  {}: {:.2f}%".format(name, ratio))
+
+    # colorize prediction
+    pred_color = colorEncode(pred, colors).astype(np.uint8)
+
+    # aggregate images and save
+    im_vis = np.concatenate((img, pred_color), axis=1)
+
+    img_name = info.split('/')[-1]
+    Image.fromarray(im_vis).save(
+        os.path.join(cfg.TEST.result, img_name.replace('.jpg', '.png')))
+    
+def run_net(segmentation_module, loader):
+    segmentation_module.eval()
+
+    for batch_data in loader:
+        # process data
+        batch_data = batch_data[0]
+        segSize = (batch_data['img_ori'].shape[0],
+                   batch_data['img_ori'].shape[1])
+        img_resized_list = batch_data['img_data']
+
+        with torch.no_grad():
+            scores = torch.zeros(1, cfg.DATASET.num_class, segSize[0], segSize[1])
+            # scores = async_copy_to(scores, gpu)
+
+            for img in img_resized_list:
+                feed_dict = batch_data.copy()
+                feed_dict['img_data'] = img
+                del feed_dict['img_ori']
+                del feed_dict['info']
+
+                # forward pass
+                pred_tmp = segmentation_module(feed_dict, segSize=segSize)
+                scores = scores + pred_tmp / len(cfg.DATASET.imgSizes)
+
+            _, pred = torch.max(scores, dim=1)
+            pred = pred.squeeze(0).cpu().numpy()
+
+        # visualization
+        visualize_result(
+            (batch_data['img_ori'], batch_data['info']),
+            pred,
+            cfg
+        )
+
+def build_net(cfg):
+    # Network Builders
+    net_encoder = ModelBuilder.build_encoder(
+        arch=cfg.MODEL.arch_encoder,
+        fc_dim=cfg.MODEL.fc_dim,
+        weights=cfg.MODEL.weights_encoder)
+    net_decoder = ModelBuilder.build_decoder(
+        arch=cfg.MODEL.arch_decoder,
+        fc_dim=cfg.MODEL.fc_dim,
+        num_class=cfg.DATASET.num_class,
+        weights=cfg.MODEL.weights_decoder,
+        use_softmax=True)
+
+    crit = nn.NLLLoss(ignore_index=-1)
+
+    segmentation_module = SegmentationModule(net_encoder, net_decoder, crit)
+
+    # Dataset and Loader
+    dataset_test = TestDataset(
+        cfg.list_test,
+        cfg.DATASET)
+    loader_test = torch.utils.data.DataLoader(
+        dataset_test,
+        batch_size=cfg.TEST.batch_size,
+        shuffle=False,
+        collate_fn=user_scattered_collate,
+        num_workers=5,
+        drop_last=True)
+
+    return segmentation_module, loader_test
+        
+def segmentation(image_path, logger):
+    config="config/ade20k-resnet50dilated-ppm_deepsup.yaml"
+    img = image_path
+    
+    cfg.merge_from_file(config)
+    
+    cfg.MODEL.arch_encoder = cfg.MODEL.arch_encoder.lower()
+    cfg.MODEL.arch_decoder = cfg.MODEL.arch_decoder.lower()
+
+    # absolute paths of model weights
+    cfg.MODEL.weights_encoder = os.path.join(
+        cfg.DIR, 'encoder_' + cfg.TEST.checkpoint)
+    cfg.MODEL.weights_decoder = os.path.join(
+        cfg.DIR, 'decoder_' + cfg.TEST.checkpoint)
+
+    assert os.path.exists(cfg.MODEL.weights_encoder) and \
+        os.path.exists(cfg.MODEL.weights_decoder), "checkpoint does not exitst!"
+
+    # generate testing image list
+    if os.path.isdir(img):
+        imgs = find_recursive(img)
+    else:
+        imgs = [img]
+    assert len(imgs), "imgs should be a path to image (.jpg) or directory."
+    cfg.list_test = [{'fpath_img': x} for x in imgs]
+
+    if not os.path.isdir(cfg.TEST.result):
+        os.makedirs(cfg.TEST.result)
+        
+    segmentation_module, loader_test = build_net(cfg)
+    run_net(segmentation_module, loader_test)
+    
+    logger = logging.getLogger(__name__)
+    logger.info("Segmentation done.")
